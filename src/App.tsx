316ecda8d8b874965e5a0da33707ef3ca16ed2b1
Natalya Botsman <botsmannatashaa@gmail.com>
2024-09-24 17:37:44 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Burger from './Components/Burger/Burger.tsx';
+
+function App() {
+
+    return (
+        <>
+            <Burger/>
+        </>
+    );
+}
+
+export default App;
---
+++ src/Components/Burger/Burger.tsx
@@ -0,0 +1,15 @@
+import '../../App.css';
+
+const Burger = () => {
+    return (
+        <div className="Burger">
+            <div className="BreadTop">
+                <div className="Seeds1"></div>
+                <div className="Seeds2"></div>
+            </div>
+            <div className="BreadBottom"></div>
+        </div>
+    );
+};
+
+export default Burger;
