13834faca7d90d6641b6a2dcbff69ce654e7458d
Natalya Botsman <botsmannatashaa@gmail.com>
2024-09-27 16:58:09 +0600

    Added logic for displaying a div with the right class by click on the AddItem button-component

--- src/Components/Burger/Burger.tsx
+++ src/Components/Burger/Burger.tsx
@@ -1,15 +1,29 @@
-import '../../App.css';
+import React, {ReactElement} from 'react';
+import {ICountIngredient} from '../../types';
+import './Burger.css';
 
-const Burger = () => {
+interface IBurger {
+    ingredients: ICountIngredient[];
+}
+
+const Burger: React.FC<IBurger> = ({ingredients}) => {
     return (
         <div className="Burger">
             <div className="BreadTop">
                 <div className="Seeds1"></div>
                 <div className="Seeds2"></div>
             </div>
+            {ingredients.map(ingredient => {
+                const item: ReactElement[] = [];
+                for (let i: number = 0; i < ingredient.count; i++) {
+                    item.push(<div className={ingredient.name} key={ingredient.name + i}></div>);
+                }
+                return item;
+            })}
             <div className="BreadBottom"></div>
         </div>
-    );
+    )
+        ;
 };
 
 export default Burger;
